bfd54f06b68033b4286222c990d29f8ce8880c4c
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-08 16:27:56 -0600

    fixes spacing in dashboard_controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,7 @@
 class DashboardController < ApplicationController
 
   def index
-    #generate list of pinned repositories
-    raw_user_repos = Faraday.get("https://api.github.com/user/repos?access_token=#{current_user.token}")
-    @user_repos = JSON.parse(raw_user_repos.body, symbolize_names: true)
-
+    @user_repos = Repo.get_repos(current_user)
     @star_count = Starred.get_count(current_user)
   end
 
