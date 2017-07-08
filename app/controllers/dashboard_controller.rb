50fc1e465ad1deb24b6939264c34b981f86789e3
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-08 12:10:20 -0600

    changes all controllers to use starred_repo_service

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,13 +4,11 @@ class DashboardController < ApplicationController
     #generate list of pinned repositories
     raw_user_repos = Faraday.get("https://api.github.com/user/repos?access_token=#{current_user.token}")
     @user_repos = JSON.parse(raw_user_repos.body, symbolize_names: true)
-    
+
     raw_user_profile = Faraday.get("https://api.github.com/user?access_token=#{current_user.token}")
     @user_profile = JSON.parse(raw_user_profile.body, symbolize_names: true)
 
-    raw_stars = Faraday.get("https://api.github.com/user/starred?access_token=#{current_user.token}")
-    parsed_stars = JSON.parse(raw_stars.body, symbolize_names: true)
-    @star_count = parsed_stars.first[:stargazers_count]
+    @star_count = Starred.get_count(current_user)
   end
 
 end
--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -7,9 +7,7 @@ class FollowingController < ApplicationController
     raw_user_profile = Faraday.get("https://api.github.com/user?access_token=#{current_user.token}")
     @user_profile = JSON.parse(raw_user_profile.body, symbolize_names: true)
 
-    raw_stars = Faraday.get("https://api.github.com/user/starred?access_token=#{current_user.token}")
-    parsed_stars = JSON.parse(raw_stars.body, symbolize_names: true)
-    @star_count = parsed_stars.first[:stargazers_count]
+    @star_count = Starred.get_count(current_user)
   end
 
 end
