4e26142b018701e47514404b1ef0d51e44cdbc5c
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-08 19:34:03 -0600

    changes repo_list from hash to object syntax

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,11 +1,12 @@
 class Repo
-  attr_reader :name, :language, :pushed, :url
+  attr_reader :name, :language, :pushed, :url, :description
 
   def initialize(repo)
     @name = repo[:name]
     @language = repo[:language]
     @pushed = repo[:pushed_at]
     @url = repo[:url]
+    @description = repo[:description]
   end
 
   def self.get_repos(user)
